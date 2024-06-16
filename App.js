b8746e9a49475fbd50faac66506b6e83d51535ed
Nhatnguyen-N <nhatnguyen13213@gmail.com>
2024-06-16 10:13:46 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native";
+import AppNavigation from "./src/navigation";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return <AppNavigation />;
+}
---
+++ src/constants/index.js
@@ -0,0 +1,33 @@
+export const categoryData = [
+  {
+    name: "Starter",
+    image: "https://www.themealdb.com/images/media/meals/vdwloy1713225718.jpg",
+  },
+  {
+    name: "Beef",
+    image: "https://www.themealdb.com/images/media/meals/vdwloy1713225718.jpg",
+  },
+  {
+    name: "Dessert",
+    image: "https://www.themealdb.com/images/media/meals/vdwloy1713225718.jpg",
+  },
+];
+
+export const mealData = [
+  {
+    name: "Starterrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr",
+    image: "https://www.themealdb.com/images/media/meals/vdwloy1713225718.jpg",
+  },
+  {
+    name: "Beef",
+    image: "https://www.themealdb.com/images/media/meals/vdwloy1713225718.jpg",
+  },
+  {
+    name: "Dessert",
+    image: "https://www.themealdb.com/images/media/meals/vdwloy1713225718.jpg",
+  },
+  {
+    name: "Dessert",
+    image: "https://www.themealdb.com/images/media/meals/vdwloy1713225718.jpg",
+  },
+];
